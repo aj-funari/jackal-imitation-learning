013d162f3cebf122bbf3474876b09da5be9287aa
mahmoud-a-ali <mahmud.a.aly@gmail.com>
2022-06-11 13:23:14 -0400

    model learning something

--- publish_action.py
+++ publish_action.py
@@ -20,7 +20,7 @@ ResNet50 = ResNet(block, [3,4,6,3], img_channels, num_classes)
 
 ### LOAD MODEL
 model = ResNet(block, [3,4,6,3], img_channels, num_classes)
-PATH = '/home/vail/aj_ws/src/jackal/models/boxes__1800%40_ResNet50_lr_0.002_wd_0.001_loss_0.8276655283239153.pt'
+PATH = '/home/vail/aj_ws/src/jackal/models/boxes__1800%40_ResNet50_lr_0.0015_wd_0.0015_loss_0.6600037713845571.pt'
 model.load_state_dict(torch.load(PATH))
 model.eval()
 model.to(torch.device('cuda'))
@@ -40,18 +40,11 @@ class data_recorder(object):
         self.rate = rospy.Rate(10)
 
     def left_img_callback(self, image):
-        '''
-        Convert ROS Image message to OpenCV2 Image
-        
-        Because I am subscribing to depth camera for testing
-        I need to convert the subscribed image from grey scale
-        to RGB. Neural network only accepts RGB images. 
-        '''
         
         # try:
         cv2_img = bridge.imgmsg_to_cv2(image)  # returns array
         img_rgb = cv2.cvtColor(cv2_img, cv2.COLOR_GRAY2RGB)
-        print("Image received and converted to RGB!")
+        # print("Image received and converted to RGB!")
 
         # resize image 
         img_resize = setup.resize(img_rgb)
@@ -67,7 +60,6 @@ class data_recorder(object):
         # feed image through neural network
         tensor_out = model(image)
         self.tensor_x_z_actions.append(tensor_out)
-        print(tensor_out)
 
         # except CvBridgeError as e:
         #     pass
@@ -82,8 +74,9 @@ class data_recorder(object):
             else:
                 if len(self.tensor_x_z_actions) >= tmp:  # publish actions only when action is sent from neural network output
                     # print("x-z actions:", self.tensor_x_z_actions[i])
-                    move.linear.x = self.tensor_x_z_actions[i][0][0]/16
-                    move.linear.z = self.tensor_x_z_actions[i][0][1]/16
+                    x = move.linear.x = self.tensor_x_z_actions[i][0][0]/32
+                    #z = move.angular.z = self.tensor_x_z_actions[i][0][1]/64
+                    print(x)
                     rospy.loginfo("Data is being sent") 
                     self.pub.publish(move)
                     self.rate.sleep()
@@ -91,7 +84,6 @@ class data_recorder(object):
                     tmp += 1
 
 if __name__ =='__main__':
-    data_recorder()
     pub = data_recorder()
     pub.publishMethod()
     rospy.spin()
